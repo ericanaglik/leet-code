411cdf80b3d2072e4bbe4d66a57eaa74cef226f0
erica2605@live.com <erica2605@live.com>
2019-06-17 10:18:35 -0700

    working on problem solving strategy

---
+++ two-sum.py
@@ -0,0 +1,28 @@
+''''''''''''''''''
+''''THE PROBLEM'''
+''''''''''''''''''
+'''Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.'''
+
+# Example:
+# Given nums = [2, 7, 11, 15], target = 9,
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+''''''''''''''''''''''''''''''''''''
+'''My problem solving strategy:'''
+''''''''''''''''''''''''''''''''''''
+
+# Step 1 -> Restate the problem in your own words:
+# Given an array of integers, find and return the indices of two numbers that add up to a target number 
+
+# Step 2 -> Ask clarifying questions:
+# What if there are no numbers that add to the target, what do I return?
+
+# Step 3 -> State your assumptions:
+# I assume that each input has exactly one solution
+# I assume i cannot use the same element twice 
+# I assume each number appears only once 
+# I assume there ARE two numbers that add up to a target and I don't have to return none or null
+
+# Step 4 -> Brainstorm solutions 
